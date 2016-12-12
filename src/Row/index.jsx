acf78465e173322708157a5245238c22b09a6c58
Steven Pietzsch <steven.pietzsch@elaxy.de>
2016-12-12 15:21:19 +0100

    Prevent warning "unknown props" in React 15

--- src/Row/index.jsx
+++ src/Row/index.jsx
@@ -37,7 +37,10 @@ module.exports = React.createClass({
     var cells = props.children || props.columns
             .map(this.renderCell.bind(this, this.props))
 
-    return <div {...props}>{cells}</div>
+    const {cellFactory, renderCell, renderText, rowHeight, minWidth,
+      rowContextMenu, showMenu, _onClick, selectable, index, columns, defaultStyle, ...rest} = props
+
+    return <div {...rest}>{cells}</div>
   },
 
   prepareProps: function(thisProps){
